1edf027b4a1dd4837625fa6249032a6fb55d6fe9
Arlen <146581650+miles3lsp@users.noreply.github.com>
2026-04-30 09:29:55 +0000

    Birthdayguest

--- guest.py
+++ guest.py
@@ -1,1 +1,9 @@
+class Guest:
+    def __init__(self, name, email, diet):
+        self.name = name
+        self.email = email
+        self.diet = diet
 
+    def __str__(self):
+      return f"{self.name} - {self.email} - {self.diet}"
+    
---
+++ main.py
@@ -0,0 +1,6 @@
+from guest import Guest
+
+g1 = Guest("Arlen", "lovmiless@gmail.com", "general")
+
+print(g1)
+
